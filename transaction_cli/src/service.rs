f56d393c342e699aa3045ebfc38d21f5fd08e9d6
LucianV <lucian.vasilut10@gmail.com>
2024-11-28 21:09:34 +0200

    added unit tests for multiple scenarios

--- transaction_cli/src/service.rs
+++ transaction_cli/src/service.rs
@@ -124,3 +124,165 @@ impl PaymentEngine {
     }
 
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::entities::Transaction;
+
+    fn create_test_transaction(tx_id: u32, client_id: u16, amount: f64, r#type: &str) -> Transaction {
+        Transaction {
+            tx: tx_id,
+            client: client_id,
+            amount: Some(amount),
+            r#type: r#type.to_string(),
+        }
+    }
+
+    #[test]
+    fn test_deposit() {
+        let tx = create_test_transaction(1, 1, 100.0, "deposit");
+        
+        let mut engine = PaymentEngine::new();
+        engine.process_transaction(tx);
+
+        let accounts = engine.get_client_accounts();
+        let current_account = accounts.get(0).unwrap();
+        assert_eq!(current_account.available_amount, 100.0);
+        assert_eq!(current_account.total_amount, 100.0);
+    }
+
+    #[test]
+    fn test_withdrawal() {
+        let mut engine = PaymentEngine::new();
+        let tx_deposit = create_test_transaction(1, 1, 100.0, "deposit");
+        engine.process_transaction(tx_deposit);
+        
+        let tx_withdrawal = create_test_transaction(2, 1, 50.0, "withdrawal");
+        engine.process_transaction(tx_withdrawal);
+
+        let accounts = engine.get_client_accounts();
+        let current_account = accounts.get(0).unwrap();
+        assert_eq!(current_account.available_amount, 50.0);
+        assert_eq!(current_account.total_amount, 50.0);
+    }
+
+    #[test]
+    fn test_withdrawal_insufficient_funds() {
+        let mut engine = PaymentEngine::new();
+        let tx_deposit = create_test_transaction(1, 1, 50.0, "deposit");
+        engine.process_transaction(tx_deposit);
+        
+        let tx_withdrawal = create_test_transaction(2, 1, 100.0, "withdrawal");
+        engine.process_transaction(tx_withdrawal);
+
+        let accounts = engine.get_client_accounts();
+        let current_account = accounts.get(0).unwrap();
+
+        // Withdrawal should be ignored, account should still have the original amount
+        assert_eq!(current_account.available_amount, 50.0);
+        assert_eq!(current_account.total_amount, 50.0);
+    }
+
+    #[test]
+    fn test_dispute() {
+        let mut engine = PaymentEngine::new();
+        let tx_deposit_one = create_test_transaction(1, 1, 100.0, "deposit");
+        let tx_deposit_two = create_test_transaction(2, 1, 25.0, "deposit");
+        engine.process_transaction(tx_deposit_one);
+        engine.process_transaction(tx_deposit_two);
+
+        let tx_dispute = create_test_transaction(2, 1, 0.0, "dispute");
+        engine.process_transaction(tx_dispute);
+
+        let accounts = engine.get_client_accounts();
+        let current_account = accounts.get(0).unwrap();
+
+        assert_eq!(current_account.available_amount, 100.0);
+        assert_eq!(current_account.held_amount, 25.0);
+
+        // Attempting a second dispute should be ignored
+        let tx_dispute = create_test_transaction(2, 1, 0.0, "dispute");
+        engine.process_transaction(tx_dispute);
+    }
+
+    #[test]
+    fn test_resolve() {
+        let mut engine = PaymentEngine::new();
+        let tx_deposit = create_test_transaction(1, 1, 100.0, "deposit");
+        engine.process_transaction(tx_deposit);
+
+        let tx_dispute = create_test_transaction(2, 1, 100.0, "dispute");
+        engine.process_transaction(tx_dispute);
+
+        let tx_resolve = create_test_transaction(3, 1, 100.0, "resolve");
+        engine.process_transaction(tx_resolve);
+
+        let accounts = engine.get_client_accounts();
+        let current_account = accounts.get(0).unwrap();
+        assert_eq!(current_account.available_amount, 100.0);
+        assert_eq!(current_account.held_amount, 0.0);
+    }
+
+    #[test]
+    fn test_chargeback() {
+        let mut engine = PaymentEngine::new();
+        let tx_deposit = create_test_transaction(1, 1, 100.0, "deposit");
+        engine.process_transaction(tx_deposit);
+
+        let tx_dispute = create_test_transaction(1, 1, 0.0, "dispute");
+        engine.process_transaction(tx_dispute);
+
+        let tx_chargeback = create_test_transaction(1, 1, 0.0, "chargeback");
+        engine.process_transaction(tx_chargeback);
+
+        let accounts = engine.get_client_accounts();
+        let current_account = accounts.get(0).unwrap();
+        assert_eq!(current_account.held_amount, 0.0);
+        assert_eq!(current_account.total_amount, 0.0);
+        assert_eq!(current_account.locked, true);
+    }
+
+    #[test]
+    fn test_chargeback_on_non_disputed() {
+        let mut engine = PaymentEngine::new();
+        let tx_deposit = create_test_transaction(1, 1, 100.0, "deposit");
+        engine.process_transaction(tx_deposit);
+
+        let tx_chargeback = create_test_transaction(1, 1, 100.0, "chargeback");
+        engine.process_transaction(tx_chargeback);
+
+        let accounts = engine.get_client_accounts();
+        let current_account = accounts.get(0).unwrap();
+
+        assert_eq!(current_account.held_amount, 0.0);
+        assert_eq!(current_account.total_amount, 100.0);
+        assert_eq!(current_account.locked, false);
+    }
+
+    #[test]
+    fn test_multiple_operations() {
+        let mut engine = PaymentEngine::new();
+        let tx_deposit = create_test_transaction(1, 1, 200.0, "deposit");
+        engine.process_transaction(tx_deposit);
+
+        let tx_withdrawal = create_test_transaction(1, 1, 100.0, "withdrawal");
+        engine.process_transaction(tx_withdrawal);
+
+        let tx_dispute = create_test_transaction(1, 1, 100.0, "dispute");
+        engine.process_transaction(tx_dispute);
+
+        let tx_resolve = create_test_transaction(1, 1, 100.0, "resolve");
+        engine.process_transaction(tx_resolve);
+
+        let tx_chargeback = create_test_transaction(1, 1, 100.0, "chargeback");
+        engine.process_transaction(tx_chargeback);
+
+        let accounts = engine.get_client_accounts();
+        let current_account = accounts.get(0).unwrap();
+
+        assert_eq!(current_account.available_amount, 100.0);
+        assert_eq!(current_account.held_amount, 0.0);
+        assert_eq!(current_account.locked, false); 
+    }
+}
